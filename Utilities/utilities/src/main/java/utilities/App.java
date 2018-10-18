a0bfe320dd4b03f55255d47760bed293eb8aaa6a
Tommaso Scalici <tommaso.scalici.1991@gmail.com>
2018-10-18 12:36:02 +0200

    Aggiunti tutti gli esercizi sulle stringhe (Esercizi 3)

--- Utilities/utilities/src/main/java/utilities/App.java
+++ Utilities/utilities/src/main/java/utilities/App.java
@@ -7,9 +7,15 @@ public final class App {
 
     public static void main(String[] args) {
 
-        String stringa = "Ho pescacascato una scarpa";
-        System.out.println(Utility.contaOccorrenze(stringa, "sca"));
-        String email = "tommaso.scalici@outlook.it";
+        String stringa = "aaaaa";
+        System.out.println(Utility.contaOccorrenze(stringa, "aa"));
+        String email = "test.email@example.com";
         System.out.println(Utility.isValidEmailWithRegex(email));
+        System.out.println(Utility.isValidEmailNoRegex(email));
+        System.out.println(Utility.paroleDiFibonacci(5));
+        System.out.println(Utility.randomString(5, 20));
+        System.out.println(Utility.sumOfDigits(1234));
+        System.out.println(Utility.eliminaVocali("Questo e un esempio che mostra come il cervello umano sia in grado di riconoscere un testo alterato"));
+        System.out.println(Utility.stringCapitalize("Questa è una stringa capitalizzata"));
     }
 }
--- Utilities/utilities/src/main/java/utilities/Utility.java
+++ Utilities/utilities/src/main/java/utilities/Utility.java
@@ -1,5 +1,6 @@
 package utilities;
 
+import java.util.Arrays;
 import java.util.regex.*;
 
 public final class Utility {
@@ -26,40 +27,152 @@ public final class Utility {
     }
 
     public static boolean isValidEmailWithRegex(String email) {
-        Pattern pattern = Pattern.compile("[A-Z0-9._]+@[A-Z0-9._]+\\.[A-Z]{2,6}", Pattern.CASE_INSENSITIVE);
+        Pattern pattern = Pattern.compile("\\w+@\\w+\\.\\w{2,6}", Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(email);
 
         return matcher.find();
     }
 
     public static boolean isValidEmailNoRegex(String email) {
-        boolean isValid = true;
-
-        int index = 0;
-
-        for (int i = 0; i < email.length(); i++) {
-            if(Character.isAlphabetic((int)email.charAt(i)) || email.charAt(i) == '.')
-                continue;
-
-            index = i;
-            break;
-        }
-
-        if(index < email.length() && email.charAt(index) == '@')
-            index++;
-        else
-            isValid = false;
-
-        for (int i = index; i < email.length(); i++) {
-            if(Character.isAlphabetic((int)email.charAt(i)))
-                continue;
-    
-            index = i;
-            break;
-        }
-
         
+        if (email == null)
+            return false;
 
-        return isValid;
+        String[] splittedString = email.split("@");
+
+        if(splittedString.length != 2)
+            return false;
+
+        for (char c : splittedString[0].toCharArray()) {
+            if(Character.isAlphabetic((int)c) || c == '.')
+                continue;
+
+            return false;
+        }
+
+        String[] splittedString2 = splittedString[1].split("\\.");
+
+        if(splittedString2.length != 2)
+            return false;
+        
+        if(splittedString2[1].length() < 2 || splittedString2[1].length() > 6)
+            return false;
+
+        for (char c : splittedString2[0].toCharArray()) {
+            if(Character.isAlphabetic((int)c))
+                continue;
+
+            return false;
+        }
+
+        for (char c : splittedString2[1].toCharArray()) {
+            if(Character.isAlphabetic((int)c))
+                continue;
+
+            return false;
+        }
+        
+        return true;
+    }
+
+    public static String paroleDiFibonacci(int n) {
+
+        if(n == 0)
+            return "";
+
+        if(n == 1)
+            return "b";
+
+        if(n == 2)
+            return "a";
+
+        String x = "b", y = "a", tmp;
+
+        for(int i = 3; i <= n; i++) {
+            tmp = x;
+            x = y;
+            y = y + tmp;
+        }
+        
+        return y;   
+
+    }
+
+    /***
+     * Genera una stringa casuale.
+     * @param a Cardinalità dell'alfabeto (da 0 a 26).
+     * @param n Lunghezza della stringa.
+     * @return La stringa casuale generata.
+     */
+    public static String randomString(int a, int n) {
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < n; i++)
+            builder.append((char)((int)(Math.random() * a) + 97));
+
+        return builder.toString();
+    }
+
+    public static int sumOfDigits(int n) {
+        
+        int quoziente = n, sommaCifre = 0;
+
+        while(quoziente > 0)
+        {
+            sommaCifre += quoziente % 10;
+            quoziente /= 10;
+        }
+
+        return sommaCifre;
+    }
+
+    public static boolean isAnagram(String s, String t) {
+        
+        char[] sArr = s.toCharArray(), tArr = t.toCharArray();
+        Arrays.sort(sArr);
+        Arrays.sort(tArr);
+
+        return sArr.equals(tArr);
+    }
+
+    public static String eliminaVocali(String s) {
+
+        String temp = s;
+
+        for (String vocal : new String[] {"a", "e", "i", "o", "u"})      
+            temp = temp.replace(vocal, "");
+
+        return temp;
+    }
+
+    public static String unary(int n) {
+        
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < n; i++)
+            builder.append(0);
+
+        return builder.toString();
+    }
+
+    public static String stringCapitalize(String s) {
+        
+        if(s == null || s.length() == 0)
+            return s;
+
+        s = s.trim();
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            
+            if(i == 0 || s.charAt(i - 1) == ' ')
+                builder.append(Character.toUpperCase(s.charAt(i)));
+            else
+                builder.append(s.charAt(i));
+        }
+
+        return builder.toString();
     }
 }
